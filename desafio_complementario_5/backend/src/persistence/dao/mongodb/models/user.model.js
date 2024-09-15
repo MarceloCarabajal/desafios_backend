0cd676af6121c96688342acc262854399b892b1a
MarceloCarabajal <marce.tsgia@gmail.com>
2024-09-15 00:27:46 -0300

    last connection and mailing

---
+++ desafio_complementario_5/backend/src/persistence/dao/mongodb/models/user.model.js
@@ -0,0 +1,31 @@
+import { Schema, model } from "mongoose";
+
+const userCollection = "users";
+
+const userSchema = new Schema({
+    first_name: { type: String, required: true},
+    last_name: { type: String, required: true},
+    email: { type: String, required: true, unique: true}, 
+    age: { type: Number }, //temporalmente borre el required:true
+    password: { type: String, required: true},
+    role: { type : String, default: 'user'},
+    image: { type: String },
+    isGithub: { type : Boolean, default: false },
+    isGoogle: { type : Boolean, default: false },
+    cart: { type: Schema.Types.ObjectId, ref: "carts", default: null },
+    last_connection: {
+        type: Date,
+        //default: Date.now,
+        //expires: 24 * 60 * 60 * 1000 // 1 day
+    },
+    active: {
+        type: Boolean,
+        default: true
+    }
+});
+
+userSchema.pre(["find", "findOne"], function() {
+    this.populate("cart");
+})
+
+export const UserModel = model(userCollection, userSchema);
